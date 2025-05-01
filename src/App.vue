27963b9a4e6b6ef0be35adfcae12a1edbf7bd216
lucasmebo <lucasmebo@hotmail.com>
2025-04-30 22:32:01 -0300

    Injetando Vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <TheHeader
+    v-show="showHeader"
+  />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js Apps"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader,
+  }, 
+  data() {
+    return {
+      showHeader: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header"> 
+     Header
+  </header>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .header {
+    background: #a2a2a2;
+    color: #fff;
+  }
+</style>
