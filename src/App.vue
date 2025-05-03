5fccb03e83eee8ddd11a898f525d1a63f544b125
lucasmebo <lucasmebo@hotmail.com>
2025-05-03 12:15:26 -0300

    Explorando Class, style e dinâmicos

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <TheHeader v-show="showHeader"/>
+ <TheHeader v-show="showHeader"/>
 
   <div v-show="showName"> 
-    Nome: {{firstName}}
-    Sobrenome: {{ lastName }}
+    Nome: {{firstName}} {{ lastName }}
   </div>
   <div v-if="acessLevel === 'admin'">Administrador</div>
   <div v-else-if="acessLevel === 'marketing'">Marketing</div>
   <div v-else>Usuário</div>
+  <img 
+    v-bind:src="imgSrc" 
+    v-bind:alt="imgAlt">
+  <div 
+    v-for="(obj, index) in todos"
+    v-bind:key="obj.id"
+    class="todos-item"
+  >
+    {{ index }} - {{ obj.title }}
+  {{ obj.title }}
+  </div>
 
-
-
-  <img alt="Vue logo" 
-  src="./assets/logo.png">
   <HelloWorld msg="To Do List App"/>
 </template>
 
@@ -31,20 +37,61 @@ export default {
       showHeader: true,
       firstName: 'Lucas',
       lastName: 'Borges',
-      showName: false,
-      acessLevel: 'admin'
+      showName: true,
+      acessLevel: 'admin',
+      imgSrc: 'https://placehold.co/150',
+      imgAlt: 'Foto do usuário',
+      todos: [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  }
+]
     }
   }
 }
 </script>
 
 <style>
+.todos-item {
+  background: #2c3e50;
+  margin: 0 0 5px 0;
+  padding: 3px 6px;
+  color: #fff;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
+
 </style>
